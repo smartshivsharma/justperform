54a0d7ecb02cb39442a61f0a020f5030cae5257e
smartshivsharma <smartshivsharma@gmail.com>
2022-05-26 07:24:06 +0530

    v5

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'line-awesome/dist/line-awesome/css/line-awesome.min.css'
 import 'font-awesome/css/font-awesome.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import '../styles/globals.css';
+// import '../styles/globals.css';
+import './globals.css';
+
 
 import Layout from '../components/layout/Layout';
 
