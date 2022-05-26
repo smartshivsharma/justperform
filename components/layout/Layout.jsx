6dd9e2d04695e6840ab364fdf098712b893ab693
smartshivsharma <smartshivsharma@gmail.com>
2022-05-26 06:42:02 +0530

    v3

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 //import { Container } from 'react-bootstrap';
 import classes from './Layout.module.css'
-import Header from './header';
+import Header from './Header';
 import Footer from './Footer';
 
 const Layout = (props) => {
