b18cee3d80f08628537c3170a6689ba8a353fc3d
smartshivsharma <96416802+smartshivsharma@users.noreply.github.com>
2022-05-31 09:27:10 +0530

    v9 changes1

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
+import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'line-awesome/dist/line-awesome/css/line-awesome.min.css'
 import 'font-awesome/css/font-awesome.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
-import '../styles/globals.css';
+
 // import './globals.css';
 
 
