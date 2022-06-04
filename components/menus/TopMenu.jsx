00d46835934b9a5bc5a549210b99d0476506c653
unknown <smartshivsharma@gmail.com>
2022-06-04 16:11:42 +0530

    changes

--- components/menus/TopMenu.jsx
+++ components/menus/TopMenu.jsx
@@ -92,7 +92,7 @@ const TopMenu = () => {
                 </NavDropdown> for second version*/}
 
 
-                <NavDropdown title="Solutions" show={show} onMouseEnter={showDropdown} onMouseLeave={hideDropdown} onClick={hideDropdown}>
+                <NavDropdown title="Solutions" show={show} onMouseEnter={showDropdown} onMouseLeave={hideDropdown} onClick={showDropdown}>
                     <Container>
                         <Row>
                             <Col lg={6} style={{ backgroundColor: '#E8F1F7' }}>
